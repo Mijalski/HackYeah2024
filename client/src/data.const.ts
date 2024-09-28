7fb3d9ea36919932b4b9c5086933d5f10ce4c5b5
wigya <amajewski505@gmail.com>
2024-09-28 22:10:36 +0200

    add country shortcuts

--- client/src/data.const.ts
+++ client/src/data.const.ts
@@ -2,14 +2,18 @@ import { icons } from "./assets/icons.const";
 
 export const LANGUAGES = [{
     language: "polish",
-    icon: icons.PL_FLAG
+    icon: icons.PL_FLAG,
+    shortcut: "pl"
 }, {
     language: "english",
-    icon: icons.US_FLAG
+    icon: icons.US_FLAG,
+    shortcut: "en"
 }, {
     language: "spanish",
-    icon: icons.ES_FLAG
+    icon: icons.ES_FLAG,
+    shortcut: "es"
 }, {
     language: "german",
-    icon: icons.DE_FLAG
+    icon: icons.DE_FLAG,
+    shortcut: "de"
 }] as const;
