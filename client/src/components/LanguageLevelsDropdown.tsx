4c873b76d1aeb1eb6945ac97e229365880a318f8
krzxs <krzy.roslon@gmail.com>
2024-09-28 22:52:30 +0200

    color fixes

--- client/src/components/LanguageLevelsDropdown.tsx
+++ client/src/components/LanguageLevelsDropdown.tsx
@@ -15,7 +15,7 @@ const LanguageLevelsDropdown = ({
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger asChild>
-        <button className="flex bg-bgPrimary space-x-4 py-2 px-4 justify-center items-center rounded-lg font-light select-none transition-colors duration-200 hover:bg-[#D0E6E8] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#A1CBD1] w-[150px]">
+        <button className="text-[#273237] flex bg-bgPrimary space-x-4 py-2 px-4 justify-center items-center rounded-lg font-light select-none transition-colors duration-200 hover:bg-[#D0E6E8] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#A1CBD1] w-[150px]">
           <p className="text-xl">{selectedLevel.level}</p>
         </button>
       </DropdownMenu.Trigger>
@@ -28,7 +28,7 @@ const LanguageLevelsDropdown = ({
           {LANGUAGE_LEVELS.map((el) => (
             <DropdownMenu.Item
               key={el.level}
-              className="flex items-center space-x-3 px-4 py-2 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none cursor-pointer transition-colors duration-200"
+              className="text-[#273237] flex items-center space-x-3 px-4 py-2 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none cursor-pointer transition-colors duration-200"
               onClick={() => handleOnClickDropdownItem(el)}
             >
               <span className="text-gray-700">{el.level}</span>
