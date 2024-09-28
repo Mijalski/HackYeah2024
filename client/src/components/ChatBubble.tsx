4f09a2acac554ee6d2932fb878d488831fcdab68
Hubert Mijalski <kyrcooler@gmail.com>
2024-09-28 19:28:02 +0200

    Chat bubbles

--- client/src/components/ChatBubble.tsx
+++ client/src/components/ChatBubble.tsx
@@ -6,24 +6,36 @@ type ChatBubbleProps = {
 
 const ChatBubble = ({ isServer = true }: ChatBubbleProps) => {
   const [userInput, setUserInput] = useState("");
+
+  // Styling for the chat bubble based on whether it's from the server or the user
+  const bubbleStyle = isServer
+    ? "bg-gray-200 text-black self-start"
+    : "bg-blue-500 text-white self-end";
+
   return (
-    <div className="w-96 min-h-14 outline-1 outline-black outline rounded-3xl px-6 py-2">
-      {isServer ? (
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-          consequuntur, voluptate soluta laudantium cupiditate dignissimos,
-          magni explicabo atque enim provident, facilis quas vel nemo deserunt
-          delectus non ratione eum quis?
-        </div>
-      ) : (
-        <div>
+    <div className={`flex ${isServer ? "justify-start" : "justify-end"} mb-2`}>
+      <div
+        className={`max-w-xs min-h-10 rounded-3xl p-4 ${bubbleStyle}`}
+        style={{ wordBreak: "break-word" }}
+      >
+        {isServer ? (
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            consequuntur, voluptate soluta laudantium cupiditate dignissimos,
+            magni explicabo atque enim provident, facilis quas vel nemo deserunt
+            delectus non ratione eum quis?
+          </div>
+        ) : (
           <input
+            className="w-full bg-transparent outline-none"
             value={userInput}
             onChange={(e) => setUserInput(e.target.value)}
+            placeholder="Type your message..."
           />
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
+
 export default ChatBubble;
