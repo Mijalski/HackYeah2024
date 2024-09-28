1b5321aa13d4a05c13661f431479246440c097ba
wigya <amajewski505@gmail.com>
2024-09-29 00:40:54 +0200

    add caching to localStorage

--- client/src/Contexts/LangSelectedContextProvider.tsx
+++ client/src/Contexts/LangSelectedContextProvider.tsx
@@ -4,6 +4,7 @@ import {
   FC,
   ReactNode,
   SetStateAction,
+  useEffect,
   useState,
 } from "react";
 import { LanguageType } from "../components/LanguagesDropdown";
@@ -41,12 +42,82 @@ export const LanguageContextProvider: FC<LanguageProviderProps> = ({
     shortcut: LANGUAGES[1].shortcut,
   });
   const [toLevel, setToLevel] = useState<LanguageLevel>({
-    level: LANGUAGE_LEVELS[1].level
+    level: LANGUAGE_LEVELS[1].level,
   });
+
+  // Read Cached Data
+  useEffect(() => {
+    const fromLangCache = localStorage.getItem("fromLanguage");
+    const toLangCache = localStorage.getItem("toLanguage");
+    const levelCache = localStorage.getItem("level");
+    console.log("toLangCache=>", toLangCache);
+    setFromLanguage(() => {
+      const foundLang = LANGUAGES.find((el) => el.language === fromLangCache);
+
+      if (foundLang?.language && foundLang?.shortcut && foundLang?.icon) {
+        return {
+          language: foundLang.language,
+          shortcut: foundLang.shortcut,
+          icon: foundLang?.icon,
+        };
+      } else {
+        return {
+          language: LANGUAGES[0].language,
+          icon: LANGUAGES[0].icon,
+          shortcut: LANGUAGES[0].shortcut,
+        };
+      }
+    });
+
+    if (toLangCache) {
+      setToLanguage(() => {
+        const foundLang = LANGUAGES.find((el) => el.language === toLangCache);
+        console.log("found to lang => ", foundLang);
+        if (foundLang?.language && foundLang?.shortcut && foundLang?.icon) {
+          return {
+            language: foundLang.language,
+            shortcut: foundLang.shortcut,
+            icon: foundLang?.icon,
+          };
+        } else {
+          return {
+            language: LANGUAGES[1].language,
+            icon: LANGUAGES[1].icon,
+            shortcut: LANGUAGES[1].shortcut,
+          };
+        }
+      });
+    }
+    if (levelCache) {
+      setToLevel({ level: levelCache ? levelCache : "A1" });
+    }
+  }, []);
+
+  // Update Cached Data
+  useEffect(() => {
+    if (
+      !(
+        fromLanguage.language === LANGUAGES[0].language &&
+        toLanguage.language === LANGUAGES[1].language &&
+        toLevel.level === LANGUAGE_LEVELS[1].level
+      )
+    ) {
+      localStorage.setItem("fromLanguage", fromLanguage.language);
+      localStorage.setItem("toLanguage", toLanguage.language);
+      localStorage.setItem("level", toLevel.level);
+    }
+  }, [fromLanguage, toLanguage, toLevel.level]);
 
   return (
     <LanguageContext.Provider
-      value={{ fromLanguage, setFromLanguage, toLanguage, setToLanguage, toLevel, setToLevel }}
+      value={{
+        fromLanguage,
+        setFromLanguage,
+        toLanguage,
+        setToLanguage,
+        toLevel,
+        setToLevel,
+      }}
     >
       {children}
     </LanguageContext.Provider>
